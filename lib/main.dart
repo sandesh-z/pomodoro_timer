9d85b5bc7efe82ee10cd753a41c65f3c52775c88
Sandace <gsandace@gmail.com>
2022-06-24 12:24:45 +0545

    bloc feature added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'package:my_pomodoro_timer/core/themes/themes.dart';
+import 'package:my_pomodoro_timer/features/pomodoro_timer_app/data/data_sources/timer.dart';
+import 'package:my_pomodoro_timer/features/pomodoro_timer_app/data/repositories/pomodoro_timer.dart';
+import 'package:my_pomodoro_timer/features/pomodoro_timer_app/domain/repositories/pomodoro_timer.dart';
+import 'package:my_pomodoro_timer/features/pomodoro_timer_app/presentation/bloc/pomodoro_bloc.dart';
 
 import 'features/pomodoro_timer_app/presentation/pages/home_page.dart';
 
@@ -13,11 +19,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: lightTheme,
-      home: HomePage(),
+    return BlocProvider<PomodoroBloc>(
+      create: (_) => PomodoroBloc(
+        pomodoroTimerRepository: PomodoroTimerRepositoryImpl(),
+      ),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: lightTheme,
+        home: HomePage(),
+      ),
     );
   }
 }
